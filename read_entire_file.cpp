db7524e0458b4c2b8de403a48c7aeaaf78210eb3
Jay Krell <jay.krell@cornell.edu>
2025-03-10 03:52:22 -0700

    wip

--- read_entire_file.cpp
+++ read_entire_file.cpp
@@ -3,20 +3,21 @@
 #include <vector>
 
 void read_entire_file(const char *file_path, std::vector<char> &contents) {
-  contents.reserve(0x10000);
+  size_t size = 0;
+  contents.resize(0x10000);
   FILE *file = fopen(file_path, "rb");
   if (!file)
     return;
   while (1) {
-    size_t bytes_to_read = (contents.capacity() - contents.size());
-    size_t bytes_read =
-        fread(&contents[contents.size()], 1, bytes_to_read, file);
+    size_t bytes_to_read = (contents.size() - size);
+    size_t bytes_read = fread(&contents[size], 1, bytes_to_read, file);
     if (bytes_read)
-      contents.resize(contents.size() + bytes_read);
+      size += bytes_read;
     if (bytes_read < bytes_to_read) {
+      contents.resize(size);
       fclose(file);
       return;
     }
-    contents.reserve(contents.capacity() * 2);
+    contents.resize(contents.size() * 2);
   }
 }
