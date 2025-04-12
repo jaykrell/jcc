d64544cc793c30e63df9e4e92ed4e9fa7205f59f
Jay Krell <jay.krell@cornell.edu>
2025-04-12 04:03:28 -0700

    wip

--- read_entire_file.h
+++ read_entire_file.h
@@ -5,6 +5,10 @@
 #include "jstdio.h"
 #include <stdint.h>
 
+#if __cplusplus
+extern "C" {
+#endif
+
 typedef struct read_entire_file_t {
   const char *file_path;
   jmalloc_t *malloc;
@@ -14,10 +18,6 @@ typedef struct read_entire_file_t {
   int out_of_memory;
   int fopen_failed;
 } read_entire_file_t;
-
-#if __cplusplus
-extern "C" {
-#endif
 
 void read_entire_file(read_entire_file_t *self);
 
