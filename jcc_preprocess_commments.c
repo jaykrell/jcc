b53686c6d43ce35d1b6a83b025365115edbb988e
Jay Krell <jay.krell@cornell.edu>
2025-05-06 22:57:01 -0700

    wip

---
+++ jcc_preprocess_commments.c
@@ -0,0 +1,43 @@
+#include "jcc.h"
+#include <assert.h>
+
+int jcc_phase3_getchar(jcc_t *jcc, int *ch)
+/* C preprocessor scanning.
+ * Read, at the phase that handles comments, turning them into spaces.
+ *
+ * Read a character. If it is not '/', return it.
+ * If it is '/', read ahead another. If that is not '*' push it back and return
+ * '/'. If it is '*', then read until closing '*' and '/', which is similar to
+ * opening. Matters such as line continuation with backward slashes are handled
+ * at a different layer.
+ * TODO: C99/C++ comments (optional, subject to command line switches)
+ */
+{
+    int err = 0;
+
+    /* If next character is definitely not opening a comment, return it. */
+    if (((err = jcc_phase2_getchar(jcc, ch))) || *ch != '/')
+        return err;
+    /* If the second character does not complete the comment start, push it back
+     * and return the first character. */
+    if (((err = jcc_phase2_getchar(jcc, ch))) || *ch != '*') {
+        jcc_phase2_unget(jcc, *ch);
+        *ch = '/';
+        return 0;
+    }
+    while (1) {
+        /* Read until end of comment, yielding a space. */
+        while (!((err = jcc_phase2_getchar(jcc, ch))) && *ch != '*')
+            ; /* nothing */
+            if (err)
+                return err;
+        assert(*ch == '*');
+        if ((err = jcc_phase2_getchar(jcc, ch)))
+            return err;
+        if (*ch == '/') {
+            *ch = ' ';
+            return 0;
+        }
+        jcc_phase2_unget(jcc, *ch); /* put back in case it is star */
+    }
+}
