2409be1216ef89b29bba94d823cb2c9b52a1783a
Jay Krell <jay.krell@cornell.edu>
2025-05-10 01:37:59 -0700

    wip

--- jcc_preprocess_line_continations.c
+++ jcc_preprocess_line_continations.c
@@ -5,11 +5,11 @@ void jcc_phase2_unget(jcc_t *jcc, int ch) {
   jcc_unget_unget(&jcc->phase2_unget, ch);
 }
 
-int jcc_phase2_getchar(jcc_t *jcc, int *ch)
+int jcc_phase2_getchar(jcc_t *jcc, int *pch)
 /* Get next character, handling line continuations.
  * This is translation phase 2.
  *
- * Each instance of a backslash character (\) immediately followed by a new-line
+ * Each instance of a backslash character (\) immediately followed by a newline
  * character is deleted, splicing physical source lines to form logical source
  * lines. Only the last backslash on any physical source line shall be eligible
  * for being part of such a splice. A source file that is not empty shall end in
@@ -18,14 +18,17 @@ int jcc_phase2_getchar(jcc_t *jcc, int *ch)
  */
 {
   int err = 0;
+  if (jcc_unget_get(&jcc->phase2_unget, pch))
+    return 0;
   while (1) {
-    if (jcc_unget_get(&jcc->phase2_unget, ch))
-      return 0;
-    if (((err = jcc_phase1_getchar(jcc, ch))) || *ch != '\\')
+    err = jcc_phase1_getchar(jcc, ch);
+    if (err || *ch != '\\')
       return err;
-    if (((err = jcc_phase1_getchar(jcc, ch))) || *ch != '\n') {
+    err = jcc_phase1_getchar(jcc, ch);
+    if (err || *ch != '\n') {
       jcc_phase1_unget(jcc, *ch);
-      return '\\';
+      *ch = '\\';
+      return err;
     }
   }
 }
