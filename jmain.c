a5aa495f8ca4aeb5dc6f4af164890c92ce9a6a93
Jay Krell <jay.krell@cornell.edu>
2025-05-25 20:26:02 -0700

    missing files

---
+++ jmain.c
@@ -0,0 +1,39 @@
+/* Combine exes into one for faster (clean) build. Link once. */
+
+#include "jmain.h"
+#include "jmax.h"
+#include <string.h>
+#include <stdio.h>
+
+void remove_exe(char* exe)
+{
+	size_t len = strlen(exe);
+	if (len <= 4)
+		return;
+	exe += len - 4;
+	if (strcmp(exe, ".exe")) return;
+	*exe = 0;
+}
+
+int main(int argc, char** argv)
+{
+	char* arg0 = argv[0];
+	char* arg1 = strrchr(arg0, '\\');
+	char* arg2 = strrchr(arg0, '/');
+	argc;
+	arg0 = JMAX(arg1, arg2);
+	if (arg0) ++arg0;
+	else arg0 = argv[0];
+	remove_exe(arg0);
+	if (!strcmp(arg0, "test_hash")) return test_hash();
+	if (!strcmp(arg0, "test_list")) return test_list();
+	if (!strcmp(arg0, "test_os")) return test_os();
+	if (!strcmp(arg0, "test_thread")) return test_thread();
+	if (!strcmp(arg0, "test_varint")) return test_varint();
+	if (!strcmp(arg0, "test_vec")) return test_vec();
+	if (!strcmp(arg0, "test_vec1")) return test_vec1();
+	if (!strcmp(arg0, "csv1")) return csv1(argc, argv);
+
+	fprintf(stderr, "ERROR: unknown argv[0]:%s\n", arg0);
+	return 1;
+}
---
+++ jmain.h
@@ -0,0 +1,18 @@
+#if __cplusplus
+extern "C" {
+#endif
+
+int test_hash(void);
+int test_list(void);
+int test_os(void);
+int test_varint(void);
+int test_vec1(void);
+int test_vec(void);
+int test_thread(void);
+
+int csv(int argc, char **argv);
+int csv1(int argc, char **argv);
+
+#if __cplusplus
+} /* extern C */
+#endif
