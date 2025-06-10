dab86984bc0ae4706d467ef8bab4be58e80d5e38
Jay Krell <jay.krell@cornell.edu>
2025-06-10 03:48:18 -0700

    start jdec

---
+++ jdec.c
@@ -0,0 +1,66 @@
+/* jcc, jlib, etc. by Jay Krell */
+/* jvec is like std::vector */
+
+#include "jvec.h"
+#include "jmax.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+void jvec_cleanup(jvec_generic *v) {
+  free(v->data);
+  /*JMEMSET0_POINTER(v);*/
+  v->data = 0;
+}
+
+int jvec_reserve(jvec_generic *v, ptrdiff_t new_capacity,
+                 ptrdiff_t element_size) {
+  ptrdiff_t capacity = v->capacity;
+  if (new_capacity > capacity) {
+    char *new_data = (char *)realloc(v->data, new_capacity * element_size);
+    if (!new_data)
+      return -1;
+    memset(new_data + v->size * element_size, 0, element_size);
+    v->data = new_data;
+    v->capacity = new_capacity;
+  }
+  return 0;
+}
+
+int jvec_resize(jvec_generic *v, ptrdiff_t new_size, ptrdiff_t element_size) {
+  int err;
+  ptrdiff_t capacity = v->capacity;
+
+  if (new_size > capacity) {
+    ptrdiff_t new_capacity = JMAX(new_size, capacity * 2);
+    if ((err = jvec_reserve(v, new_capacity, element_size)))
+      return err;
+  }
+  v->size = new_size;
+  return 0;
+}
+
+int jvec_insert(jvec_generic *v, void const *before, void const *begin,
+                ptrdiff_t count, ptrdiff_t element_size) {
+  ptrdiff_t before_offset = ((char const *)before - v->data);
+  ptrdiff_t new_size = v->size + count;
+
+  int err = jvec_resize(v, new_size, element_size);
+  if (err)
+    return err;
+  memmove(v->data + before_offset + (count * element_size),
+          v->data + before_offset, count * element_size);
+  memmove(v->data + before_offset, begin, count * element_size);
+  return 0;
+}
+
+int jvec_push_back(jvec_generic *v, void const *element,
+                   ptrdiff_t element_size) {
+  ptrdiff_t size = v->size;
+  int err = jvec_resize(v, size + 1, element_size);
+  if (err)
+    return err;
+
+  memcpy(v->data + size * element_size, element, element_size);
+  return 0;
+}
---
+++ jdec.h
@@ -0,0 +1,60 @@
+/* jvec is like std::vector
+ * jvec1 is earlier version, probably not to use. */
+#if !JVEC_H
+#define JVEC_H 1
+
+#include "jssize.h"
+#include <stdint.h>
+
+#if __cplusplus
+extern "C" {
+#endif
+
+/* In order to be idiomatic to user, size and capacity are in elements, not
+ * bytes users rarely/never use capacity, but they will use and this is
+ * consistent between them. We may come to regret this, i.e. avoid
+ * multiplication or division in the library and stick division by constant in
+ * user. */
+#define JVEC(T)                                                                \
+  struct {                                                                     \
+    T *data;                                                                   \
+    ptrdiff_t size;                                                            \
+    ptrdiff_t capacity;                                                        \
+    int static_;                                                               \
+    T *prealloc;                 /*TODO*/                                      \
+    ptrdiff_t prealloc_capacity; /*TODO*/                                      \
+  }
+/*TODO: size_t? */
+
+typedef JVEC(char) jvec_generic;
+
+void jvec_cleanup(jvec_generic *);
+int jvec_push_back(jvec_generic *, void const *element, ptrdiff_t element_size);
+int jvec_resize(jvec_generic *, ptrdiff_t new_size, ptrdiff_t element_size);
+int jvec_reserve(jvec_generic *, ptrdiff_t new_size, ptrdiff_t element_size);
+int jvec_insert(jvec_generic *, void const *before, void const *begin,
+                ptrdiff_t count, ptrdiff_t element_size);
+
+#define JVEC_CLEANUP(v) jvec_cleanup((jvec_generic *)(v))
+
+#define JVEC_RESIZE(v, size)                                                   \
+  jvec_resize((jvec_generic *)(v), size, sizeof((v)->data[0]))
+
+#define JVEC_END(v) ((v)->data + (v)->size)
+#define JVEC_PUSH_BACK(v, element)                                             \
+  jvec_push_back((jvec_generic *)(v), element, sizeof((v)->data[0]))
+
+#define JVEC_INSERT(v, before, begin, count)                                   \
+  jvec_insert((jvec_generic *)(v), (before), (begin), (count),                 \
+              sizeof((v)->data[0]))
+#define JVEC_APPEND(v, begin, count) JVEC_INSERT(v, JVEC_END(v), begin, count)
+#define JVEC_RESERVE(v, n)                                                     \
+  jvec_reserve((jvec_generic *)(v), (n), sizeof((v)->data[0]))
+
+typedef JVEC(char) jvec_char_t;
+typedef JVEC(uint64_t) jvec_uint64_t;
+
+#if __cplusplus
+}
+#endif
+#endif
